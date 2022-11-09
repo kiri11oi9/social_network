ebd7a9ce071bd7b877a8ee9583eb7c08f3e1ed1c
kiri110i9 <Kirilloid999@yandex.ru>
2022-11-09 16:34:44 +0300

    Заливка проекта на GitHub

---
+++ my-app/src/components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+    return (
+        <header className='header'>
+            <img
+                src="https://w7.pngwing.com/pngs/730/664/png-transparent-logo-social-media-brand-instagram-logo-text-internet-symbol-thumbnail.png"
+                alt=""/>
+        </header>
+    );
+}
+
+export default Header;
---
+++ my-app/src/components/Profile.jsx
@@ -0,0 +1,28 @@
+const Profile = () =>{
+    return(
+        <div className='content'>
+            <div>
+                <img
+                    src="https://celes.club/uploads/posts/2021-11/1637669611_28-celes-club-p-malenkie-kroliki-zhivotnie-krasivo-foto-30.jpg"
+                    alt=""/>
+            </div>
+            <div>
+                ava+desc
+            </div>
+            <div>
+                my posts
+                <div>
+                    new post
+                </div>
+                <div>
+                    post1
+                </div>
+                <div>
+                    post2
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
